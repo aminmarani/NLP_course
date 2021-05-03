78ff00f258090b6f1055c346fd7392ebe103e96d
amin hosseiny marani <amh418@lehigh.edu>
2021-05-02 23:42:57 -0400

    =solving project 3

--- Projects/project_3_release/src/problem1.py
+++ Projects/project_3_release/src/problem1.py
@@ -24,8 +24,60 @@ class CFG:
         :param file_name: path to the file containing the grammar.
         :return:
         """
-        #########################################
-        ## INSERT YOUR CODE HERE
+        ########################################
+        with open(file_name,'r') as f:
+            for line in f:
+                if (line[0] != '#' and len(line)>3): #if this line starts with '#' or is shorter than length of 3 skip it
+                    line = line[0:-1] #removing \n
+                    arr = line.split('\t')
+                    #there is a bug in grammar and two last rules are separated using two spaces not tabs
+                    if len(arr) == 1:
+                        arr = line.split('   ')
+                    #if len(arr[2].split(' ')) > 1: #the rhs consists of 2 non-terminals
+                    #adding lhs2rhs
+                    if arr[1] in self.lhs2rhs.keys(): #an exisiting key
+                        t = self.lhs2rhs[arr[1]]
+                        t.append([' '.join(arr[2:3]),float(arr[0])])
+                        self.lhs2rhs[arr[1]] = t
+                    else: #a new key
+                        self.lhs2rhs[arr[1]] = [[' '.join(arr[2:3]),float(arr[0])]]
+                        #adding non-terminal
+                        if arr[1].isupper(): #a Non-terminal has all upper-case letters
+                            self.non_terminals.add(arr[1])
+
+                    if len(arr[2].split(' ')) == 1:#len(arr) == 3: #terminal grammar --> add word2tag
+                        if arr[2] in self.word2tag.keys() and arr[1] not in self.word2tag[arr[2]]:
+                            t = self.word2tag[arr[2]]
+                            t.append(arr[1])
+                            self.word2tag[arr[2]] = t
+                        elif arr[2] not in self.word2tag.keys():
+                            self.word2tag[arr[2]] = [[arr[1]]]
+                            #time to add a terminal
+                            self.terminals.add(arr[2])
+                        #add postags
+                        self.pos_tags.add(arr[1])
+
+                    #adding rhs2lhs
+                    rhs_key = ' '.join(arr[2:3])
+                    if  rhs_key in self.rhs2lhs.keys():
+                        t = self.rhs2lhs[rhs_key]
+                        t.append(arr[1])
+                        self.rhs2lhs[rhs_key] = t
+                    else:
+                        self.rhs2lhs[rhs_key] = [arr[1]]
+
+
+        #go over self.lhs2rhs and recompute all probabilities
+        for ky in self.lhs2rhs.keys():
+            s = 0
+            for i in range(len(self.lhs2rhs[ky])):
+                s += self.lhs2rhs[ky][i][1]
+            for i in range(len(self.lhs2rhs[ky])):
+                self.lhs2rhs[ky][i][1] = self.lhs2rhs[ky][i][1] / s
+            #time to sort every single item of this key
+            self.lhs2rhs[ky] = sorted(self.lhs2rhs[ky],key= lambda x: x[1] ,reverse=True)
+
+
         #########################################
 
     def symbol_type(self, symbol):
@@ -36,5 +88,17 @@ class CFG:
         if it is a POS-tag, return 1
         """
         #########################################
-        ## INSERT YOUR CODE HERE
+        if symbol in self.terminals:
+            return 0
+        if symbol in self.non_terminals:
+            return 2
+        if symbol in self.pos_tags:
+            return 1
+        return -1
         #########################################
+
+
+
+grammar_path = '../data/grammar.gr'
+cfg = CFG()
+cfg.load_grammar(grammar_path)
--- Projects/project_3_release/src/problem2.py
+++ Projects/project_3_release/src/problem2.py
@@ -1,5 +1,6 @@
 import random
 from problem1 import *
+import numpy as np
 
 class Node:
     def __init__(self, _symbol):
@@ -24,7 +25,14 @@ class Generator:
         :param node: the node to be expanded
         """
         #########################################
-        ## INSERT YOUR CODE HERE
+        out = self.select_rule(node.symbol)
+        if out != None: #expand the node
+            out = out.split(' ') #some rhs are 2 non-terminals and some are 1 terminals
+            node.leftChild = Node(out[0])
+            self.grow_node(node.leftChild)
+            if len(out) == 2: #there is another child to expand
+                node.rightChild = Node(out[1])
+                self.grow_node(node.rightChild)
         #########################################
 
     def print_node(self, node:Node):
@@ -33,7 +41,20 @@ class Generator:
         :param node: the node to be expanded
         """
         #########################################
-        ## INSERT YOUR CODE HERE
+        if node.leftChild != None and node.rightChild != None:
+            s = '(' + node.symbol
+        #print(node.symbol)
+        #if node.leftChild != None:
+            s+= ' '+ self.print_node(node.leftChild)
+        #if node.rightChild != None:
+            s+= ' ' + self.print_node(node.rightChild)
+
+            s += ')'
+
+            return s
+        else:
+            return '(' + node.symbol + ' ' + node.leftChild.symbol + ')'
+
         #########################################
 
     def select_rule(self, lhs):
@@ -42,5 +63,35 @@ class Generator:
         :param lhs: the given lhs. Random selection using the rule probabilities if self.random_selection = True
         """
         #########################################
-        ## INSERT YOUR CODE HERE
+        rnd = np.random.rand()
+
+        #return None #if the lhs does not exist
+        if lhs not in self.cfg.lhs2rhs.keys():
+            return None
+
+        if not self.random_selection : #return the top one
+            return self.cfg.lhs2rhs[lhs][0][0]
+        s = 0 #sum of random numbers
+        for items in self.cfg.lhs2rhs[lhs]:
+            s += items[1]
+            if rnd <= s:
+                return items[0]
+        
         #########################################
+
+
+
+grammar_path = '../data/grammar.gr'
+cfg = CFG()
+cfg.load_grammar(grammar_path)
+# set randomness to False for testing purpose
+# to generate interesting sentences, set randomness = True
+gen = Generator(cfg, False)
+
+for lhs in cfg.lhs2rhs.keys():
+    rhss = [rhs for rhs, count in cfg.lhs2rhs[lhs]]
+    rhs = gen.select_rule(lhs)
+
+root = Node('ROOT')
+gen.grow_node(root)
+print(gen.print_node(root))
